fbd8136cbf063a5d2d223bbac88edfaeefaced6b
vlnarvaez <vlnarvaez@utpl.edu.ec>
2015-06-23 23:07:54 -0500

    ventana emergente

---
+++ Online/static/detalle_producto.php
@@ -0,0 +1,2 @@
+<div id="light" class="white_content">This is the lightbox content. <a href = "javascript:void(0)" onclick = "document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">Close</a></div>
+		<div id="fade" class="black_overlay"></div>
