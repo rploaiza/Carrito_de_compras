05de718e8c5a472b8b77f65dec99f2a3fb2b9882
vlnarvaez <vlnarvaez@utpl.edu.ec>
2015-06-29 20:00:55 -0500

    Update footer.php

--- Online/static/footer.php
+++ Online/static/footer.php
@@ -3,21 +3,28 @@
 
 <div class="col-md-4">
   <section>
-    <img id="marca" style="width: 50px;" src="img/mac.png">
-    <img id="marca" style="width: 50px;" src="img/toshiba.png">
-    <img id="marca" style="width: 50px;" src="img/Sony.png">
+    <img id="marca" style="width: 65px;" src="img/mac.png">
     <img id="marca" style="width: 50px;" src="img/hp.png">
-    <img id="marca" style="width: 50px;" src="img/epson.png">
     <img id="marca" style="width: 50px;" src="img/dell.png">
+    <img id="marca" style="width: 50px;" src="img/Sony.png"><br>
     <img id="marca" style="width: 50px;" src="img/Canon.png">
     <img id="marca" style="width: 50px;" src="img/acer.png">
-    <img id="marca" style="width: 50px;" src="img/samsung.png">
+    <img id="marca" style="width: 60px;" src="img/samsung.png"><br>
+    <img id="marca" style="width: 50px;" src="img/toshiba.png">
+    <img id="marca" style="width: 50px;" src="img/epson.png">
   </section>
 </div>
   
 
   <div class="col-md-4">
-  <div class="foot">
+      <img style="width: 40%; padding-top: 3%;" src="img/utpl.png">
+      <p style=" padding-top:6%; color:#fff; font-size:90%;">@lcchalan @rploaiza @gagutierrez @sapupiales</p>
+  </div>
+
+
+<div class="col-md-4">
+
+    <div class="foot">
     <ul class="social-network social-circle">
                         <li><a id="fot" href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
                         <li><a id="fot" href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
@@ -25,15 +32,6 @@
                         <li><a id="fot" href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-instagram"></i></a></li>
                     </ul>               
                 </div>
-  </div>
-
-
-<div class="col-md-4">
-    <p>@vlnarvaez</p>
-    <p>@lcchalan</p>
-    <p>@gagutierrez</p>
-    <p>@rploaiza</p>
-    <p>@sapupiales</p>
   </div>
 
 </div>
