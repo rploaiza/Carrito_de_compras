3c667eccd47e4533e0cad732f549a5ba7a1210ad
vlnarvaez <vlnarvaez@utpl.edu.ec>
2015-06-29 20:06:17 -0500

    Update index.php

--- Online/index.php
+++ Online/index.php
@@ -17,14 +17,15 @@ if(!empty($_GET['del'])){
 
   <!-- Le styles -->
   <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/agency.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap-responsive.css" rel="stylesheet">
   <style type="text/css">
     body {
       padding-top: 60px;
       padding-bottom: 40px;
     }
   </style>
-  <link href="css/bootstrap-responsive.css" rel="stylesheet">
-  <link href="css/agency.css" rel="stylesheet">
   <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="../assets/js/html5shiv.js"></script>
@@ -67,14 +68,56 @@ if(!empty($_GET['del'])){
     </head>
 
     <body>
+        <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div  class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Menu de Navegación</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="principal.php"></a>
+                <!--<a class="navbar-brand page-scroll" href="#page-top" style="margin-left:45%;";>LINE BUY</a> -->
+            </div>
 
-      <nav>
-        <?php include("static/nav.php") ?>
-      </nav>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul style="margin-top: -4%;" style="float: right;" class="nav navbar-nav navbar-right">
+                
+                <img style="width: 10%; float: left;" src="img/logo.png">
+
+                <li style="float: right;  width: 10%;">
+                        <a style="font-size: 90%;" class="page-scroll" href="principal.php"><img style="width: 150%;" id="home" src="ico/home.png">Inicio</a>
+                    </li>
+
+                <li style="float: right;  width: 10%;">
+                        <a style="font-size: 90%;" class="page-scroll" href="index.php"><img style="width: 150%;" id="carrito" src="ico/carrito.png">Comprar</a>
+                    </li>     
+
+                <li style="float: right;  width: 10%;">
+                        <a style="font-size: 90%;" class="page-scroll" href="mis_pedidos.php"><img style="width: 500%;" id="carrito" src="ico/pedidos.png">Pedidos</a>
+                    </li>   
+
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+    </nav>
+
       <div class="row-fluid">
 
 
         <div class="span2">
+
+        <div class="col-md-2">
+            <br>
+            <br>
+            <button name='btn_cat' class='btn btn-xl1' data-filter=' '><a href='index.php' class='nav$a' data-type=' '>Categorias</a></button>
+            <button name='btn_cat' class='btn btn-xl1' data-filter=' '><a href='index.php' class='nav$a' data-type=' '>Categorias</a></button>
+            <button name='btn_cat' class='btn btn-xl1' data-filter=' '><a href='index.php' class='nav$a' data-type=' '>Categorias</a></button>
+
+        </div>
         
         </div>
 
