3fa8497510beada4567ea0645271e08ecf00355e
vlnarvaez <vlnarvaez@utpl.edu.ec>
2015-06-29 19:59:50 -0500

    Update detalle_producto.php

--- Online/static/detalle_producto.php
+++ Online/static/detalle_producto.php
@@ -1,2 +1,8 @@
-<div id="light" class="white_content">This is the lightbox content. <a href = "javascript:void(0)" onclick = "document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">Close</a></div>
-		<div id="fade" class="black_overlay"></div>
+<div id="light" class="white_content">
+	<a href = "javascript:void(0)" onclick = "document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">
+		<button type="button" class="close" aria-label="Close">
+			<span  style="color:#0044cc;" aria-hidden="true">&times;</span>
+		</button>
+	</a>
+</div>
+<div id="fade" class="black_overlay"></div>
