af986223d7d4a7ec584f35edb0875c0f8f3a8941
Vanessa Narváez <vlnarvaez@utpl.edu.ec>
2015-07-07 11:38:27 -0500

    lll

--- Online/static/clase_mysql.php
+++ Online/static/clase_mysql.php
@@ -491,16 +491,42 @@ function hora(){
 
 
 function procategoria(){
-	echo "Categoria: ";
-	$query = "SELECT * FROM categoria_producto WHERE id";
-	$result = mysql_query($query) or die("error". mysql_error());                                   
-	echo "<div class='form-group'>";                                                                
-	echo "<select class='form-control' name='idcat'>";
-	echo '<option value="" default selected>Seleccione</option>';
-	while ($row = mysql_fetch_array($result)) {
-		echo "<h3 class='section-subheading text-muted'> string   ".$row[0]."</h3>";   
-		echo "string  ".$row[0];
-		echo "<option value='".$row[0]."'>".$row[1]."</option>"; 
+		echo "Categoria: ";
+		$query = "SELECT * FROM categoria_producto WHERE id";
+		$result = mysql_query($query) or die("error". mysql_error());                                   
+		echo "<div class='form-group'>";                                                                
+		echo "<select class='form-control' name='idcat'>";
+		echo '<option value="" default selected>- Select -</option>';
+		while ($row = mysql_fetch_array($result)) {
+			echo "<h3 class='section-subheading text-muted'> string   ".$row[0]."</h3>";   
+			echo "string  ".$row[0];
+			echo "<option value='".$row[0]."'>".$row[1]."</option>"; 
+		}
+		echo "</select><br>";
+		echo "<p class='help-block text-danger'></p>";
+		echo "</div>";
+	                                                    //------- extraccion de los estados ------
+		include ("static/estado.php");
+
+		echo "<select class='form-control' name='estado'>";
+		echo '<option value=""> - Select - </option>';
+		echo '<option value="s">Disnonible</option>';
+		echo '<option value="s">Fuera de stock</option>';
+		echo "</select><br>";
+
+  	                                                    //-------- exit -------
+		$query = "SELECT codigo, nombre, marca, nota, valor, cantidad FROM producto";
+		$result = mysql_query($query) or die("error". mysql_error());
+		$a = mysql_num_fields($result);
+		while ($row = mysql_fetch_array($result)) {
+			for ($i=0; $i < $a ; $i++) { 
+				echo "<div class='form-group'>";
+				echo mysql_field_name($result, $i).":<input class='form-control' name='".mysql_field_name($result, $i)."' type='text' placeholder='".mysql_field_name($result, $i)."'>";
+				echo "<p class='help-block text-danger'></p>";
+				echo "</div>";
+			}
+			$a=0;                             
+		} 
 	}
 		function procategoria2(){
 			echo "Categoria: ";
@@ -539,69 +565,8 @@ function procategoria(){
 				echo "</div>";
 			}
 			$a=0;                             
-	include ("static/estado.php");
-    $_POST['idcatest'];
-	echo "Estado del producto: ";
-	echo "<select class='form-control' name='estado'>";
-	echo '<option value=""> - Select - </option>';
-	echo '<option value="s">Disnonible</option>';
-	echo '<option value="n">Fuera de stock</option>';
-	echo "</select><br>";
-	$query = "SELECT codigo, nombre, marca, nota, valor, estado, cantidad FROM producto";
-	$result = mysql_query($query) or die("error". mysql_error());
-	$a = mysql_num_fields($result);
-	while ($row = mysql_fetch_array($result)) {
-		for ($i=0; $i < $a ; $i++) { 
-			echo "<div class='form-group'>";
-			echo mysql_field_name($result, $i).":<input class='form-control' name='".mysql_field_name($result, $i)."' type='text' placeholder='".mysql_field_name($result, $i)."'>";
-			echo "<p class='help-block text-danger'></p>";
-			echo "</div>";
 		}
-		$a=0;                              
 	}
-}
-
-function procategoria2(){
-	echo "Categoria: ";
-	$query = "SELECT * FROM categoria_producto WHERE id";
-	$result = mysql_query($query) or die("error". mysql_error());                                   
-	echo "<div class='form-group'>";                                                                
-	echo "<select class='form-control' name='idcat'>";
-	echo '<option value="" default selected>- Select -</option>';
-	while ($row = mysql_fetch_array($result)) {
-		echo "<h3 class='section-subheading text-muted'> string   ".$row[0]."</h3>";   
-		echo "string  ".$row[0];
-		echo "<option value='".$row[0]."'>".$row[1]."</option>"; 
-	}
-	echo "</select><br>";
-	echo "<p class='help-block text-danger'></p>";
-	echo "</div>";
-                                                    //------- extraccion de los estados ------
-	include ("static/estado.php");
-    $_POST['idcatest'];
-	echo "Estado del producto: ";
-	echo "<select class='form-control' name='estado'>";
-	echo '<option value=""> - Select - </option>';
-	echo '<option value="s">Disnonible</option>';
-	echo '<option value="n">Fuera de stock</option>';
-	echo "</select><br>";
-}
-
-function catprod(){
-	$query = "SELECT categoria FROM categoria_producto";
-	$result = mysql_query($query) or die("error". mysql_error());
-	$a = mysql_num_fields($result);
-	while ($row = mysql_fetch_array($result)) {
-		for ($i=0; $i < $a ; $i++) { 
-			echo "<div class='form-group'>";
-			echo mysql_field_name($result, $i).":<input class='form-control' name='".mysql_field_name($result, $i)."' type='text'>";
-			echo "<p class='help-block text-danger'></p>";
-			echo "</div>";
-		}
-		$a=0;                             
-	}
-
-}
 function categoria_estado(){
 	$query = "SELECT estado FROM categoria_estado";
 	$result = mysql_query($query) or die("error". mysql_error());
