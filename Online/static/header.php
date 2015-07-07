6b7b5929a22a173ec9452b9b7b48fded7844a577
Vanessa Narváez <vlnarvaez@utpl.edu.ec>
2015-07-07 13:10:11 -0500

    header

--- Online/static/header.php
+++ Online/static/header.php
@@ -7,6 +7,12 @@
                 <li><div data-image="2/md/1.jpg"></div></li>
                 <li><div data-image="2/md/2.jpg"></div></li>
                 <li><div data-image="2/md/3.jpg"></div></li>
+                <li><div data-image="2/md/4.jpg"></div></li>
+                <li><div data-image="2/md/5.jpg"></div></li>
+                <li><div data-image="2/md/6.jpg"></div></li>
+                <li><div data-image="2/md/7.jpg"></div></li>
+                <li><div data-image="2/md/8.jpg"></div></li>
+                <li><div data-image="2/md/9.jpg"></div></li>
             </ul>
         </div>
         <!--end-->
