e6d01d6d82383dd248895b80027fbf3c56e7d087
Vanessa Narváez <vlnarvaez@utpl.edu.ec>
2015-07-19 12:49:41 -0500

    catalogo

--- Online/principal.php
+++ Online/principal.php
@@ -157,26 +157,21 @@ $miconexion->conectar($db_name,$db_host, $db_user,$db_password);
                                 }
                                 
                                 ?>
-                                <div id="light" class="white_content">
-                                  <a href = "javascript:void(0)" onclick = "document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">
-                                    <button type="button" class="close" aria-label="Close">
-                                      <span  style="color:#0044cc;" aria-hidden="true">&times;</span>
-                                    </button>
-                                  </a>
+
+                                <div id="modal1" class="modalmask">
+
+                                <div class="modalbox movedown">
+
+                                    <a href="principal.php" title="Close" class="close">X</a>
+                                    <?php
+                                        $miconexion->consulta("select * from producto where id=".$_GET['id']);
+                                        $miconexion->descatalogo();
+                                        ?>
+                                    </div>
                                 </div>
-                                <div id="fade" class="black_overlay"></div>
                             
                               <!-- Fin catalogo -->
-            
-                            <div id="light" class="white_content">
-                                <a href = "javascript:void(0)" onclick = "document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">
-                                    <button type="button" class="close" aria-label="Close">
-                                        <span  style="color:#0044cc;" aria-hidden="true">&times;</span>
-                                    </button>
-                                </a>
-                            </div>
-                            <div id="fade" class="black_overlay"></div>
-                            <!-- Fin catalogo -->
+    
                             </section>
                         </aside>
                     </div>
